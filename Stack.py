333e8d81c7d2bc8e823ba8d772da04cfee7cff62
vedantronkhede <ronkhedevedant36@gmail.com>
2024-03-10 13:57:39 +0530

    Updated stack code

--- Stack.py
+++ Stack.py
@@ -1,3 +1,97 @@
+# Python program to
+# demonstrate stack implementation
+# using list
+ 
+stack = []
+ 
+# append() function to push
+# element in the stack
+stack.append('a')
+stack.append('b')
+stack.append('c')
+ 
+print('Initial stack')
+print(stack)
+ 
+# pop() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+ 
+print('\nStack after elements are popped:')
+print(stack)
+ 
+# uncommenting print(stack.pop())
+# will cause an IndexError
+# as the stack is now empty
+
+# Python program to
+# demonstrate stack implementation
+# using collections.deque
+ 
+from collections import deque
+ 
+stack = deque()
+ 
+# append() function to push
+# element in the stack
+stack.append('a')
+stack.append('b')
+stack.append('c')
+ 
+print('Initial stack:')
+print(stack)
+ 
+# pop() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+ 
+print('\nStack after elements are popped:')
+print(stack)
+ 
+# uncommenting print(stack.pop())
+# will cause an IndexError
+# as the stack is now empty
+
+# Python program to
+# demonstrate stack implementation
+# using queue module
+ 
+from queue import LifoQueue
+ 
+# Initializing a stack
+stack = LifoQueue(maxsize=3)
+ 
+# qsize() show the number of elements
+# in the stack
+print(stack.qsize())
+ 
+# put() function to push
+# element in the stack
+stack.put('a')
+stack.put('b')
+stack.put('c')
+ 
+print("Full: ", stack.full())
+print("Size: ", stack.qsize())
+ 
+# get() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from the stack')
+print(stack.get())
+print(stack.get())
+print(stack.get())
+ 
+print("\nEmpty: ", stack.empty())
+
 # Python program to demonstrate
 # stack implementation using a linked list.
 # node class
